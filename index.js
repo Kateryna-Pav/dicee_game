70a974c08af645a36880b7c478f06d012fba3ec6
Kateryna-Pav <74582255+Kateryna-Pav@users.noreply.github.com>
2021-02-03 18:27:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var randomNumber1 = Math.floor((Math.random()*6)+1);
 var randomNumber2 = Math.floor((Math.random()*6)+1);
 
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+var player1 = prompt("Player1 Name: ");
+var player2 = prompt("Player2 Name: ");
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+document.querySelector(".player1").innerHTML = player1;
+document.querySelector(".player2").innerHTML = player2;
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML="🚩 Player 1 wins!";
+    document.querySelector("h1").innerHTML="🚩Player 1 wins!";
+    document.querySelector(".player1").style.color = "red";
+    document.querySelector(".player1").style.fontStyle = "italic";
+
 }
 else if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML="Player 2 wins! 🚩";
+    document.querySelector(".player2").style.color = "red";
+    document.querySelector(".player2").style.fontStyle = "italic";
 }
 
 else {
