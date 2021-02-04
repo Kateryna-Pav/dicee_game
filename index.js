38c953a6d358c882953251e8c2c1141c6624ba43
Kateryna-Pav <74582255+Kateryna-Pav@users.noreply.github.com>
2021-02-03 19:32:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-var randomNumber1 = Math.floor((Math.random()*6)+1);
-var randomNumber2 = Math.floor((Math.random()*6)+1);
+var player1Name = prompt("Player1 Name: ");
+var player2Name = prompt("Player2 Name: ");
 
-var player1 = prompt("Player1 Name: ");
-var player2 = prompt("Player2 Name: ");
+var player1 = player1Name.toUpperCase();
+var player2 = player2Name.toUpperCase();
 
-document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+var refreshButton = document.querySelectorAll(".refresh-btn").length;
 
-document.querySelector(".player1").innerHTML = player1;
-document.querySelector(".player2").innerHTML = player2;
+for (var i = 0; i < refreshButton; i++) {
+    document.querySelectorAll(".refresh-btn")[i].addEventListener("click", function () {
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML="🚩Player 1 wins!";
-    document.querySelector(".player1").style.color = "red";
-    document.querySelector(".player1").style.fontStyle = "italic";
+        var buttonInnerHTML = this.innerHTML;
+        refresh(buttonInnerHTML);
+    });
 
 }
-else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML="Player 2 wins! 🚩";
-    document.querySelector(".player2").style.color = "red";
-    document.querySelector(".player2").style.fontStyle = "italic";
-}
 
-else {
-    document.querySelector("h1").innerHTML="🎌 Draw 🎌";
+
+function refresh() {
+
+    var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+    var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    document.querySelector(".player1").innerHTML = player1;
+    document.querySelector(".player2").innerHTML = player2;
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+        document.querySelector(".player1").style.color = "red";
+        document.querySelector(".player1").style.fontStyle = "italic";
+        document.querySelector(".player1").style.fontWeight = "bold";
+        document.querySelector(".player2").style.color = "#79a3b1";
+        document.querySelector(".player2").style.fontStyle = "none";
+        document.querySelector(".player2").style.fontWeight = "none";
+
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+        document.querySelector(".player2").style.color = "red";
+        document.querySelector(".player2").style.fontStyle = "italic";
+        document.querySelector(".player2").style.fontWeight = "bold";
+        document.querySelector(".player1").style.color = "#79a3b1";
+        document.querySelector(".player1").style.fontStyle = "none";
+        document.querySelector(".player1").style.fontWeight = "none";
+
+    } else {
+        document.querySelector("h1").innerHTML = "🎌 Draw 🎌";
+        document.querySelector(".player2").style.color = "#79a3b1";
+        document.querySelector(".player2").style.fontStyle = "none";
+        document.querySelector(".player2").style.fontWeight = "none";
+        document.querySelector(".player1").style.color = "#79a3b1";
+        document.querySelector(".player1").style.fontStyle = "none";
+        document.querySelector(".player1").style.fontWeight = "none";
+
+    }
 }
